5c67dd921f187614b7cc8c88169b79e62d6be9a0
Takahiro555555 <tkhr.corei7@gmail.com>
2020-02-10 04:20:01 +0900

    Add: すべてのPythonスクリプトを実行する

---
+++ src/run_all.sh
@@ -0,0 +1,9 @@
+#! /bin/zsh
+
+poetry run python3 report01.py
+poetry run python3 report02.py
+poetry run python3 report03.py
+poetry run python3 report04.py
+poetry run python3 report05_m2.py
+poetry run python3 report05_m3.py
+poetry run python3 report05_m5.py
