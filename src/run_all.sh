ad8d7b31a0dc1c20f9820a68064219c89feeba11
Takahiro55555 <tkhr.corei7@gmail.com>
2020-02-13 04:44:26 +0900

    Update: 実行結果をファイルに保存するように変更

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,9 +1,41 @@
 #! /bin/zsh
 
-poetry run python3 report01.py
-poetry run python3 report02.py
-poetry run python3 report03.py
-poetry run python3 report04.py
-poetry run python3 report05_m2.py
-poetry run python3 report05_m3.py
-poetry run python3 report05_m5.py
+time_stamp=`date +"%Y%m%d-%H%M%S"`;
+file_name="result-${time_stamp}.zip"
+
+#shasum report01.py > $file_name
+#poetry run python3 report01.py >> $file_name
+#shasum ../img/p1.png >> $file_name
+#echo "" >> $file_name
+
+shasum report02.py > $file_name
+poetry run python3 report02.py >> $file_name
+shasum ../img/p2.png >> $file_name
+echo "" >> $file_name
+
+shasum report03.py >> $file_name
+poetry run python3 report03.py >> $file_name
+shasum ../img/p3.png >> $file_name
+echo "" >> $file_name
+
+shasum report04.py >> $file_name
+poetry run python3 report04.py >> $file_name
+shasum ../img/p4.png >> $file_name
+echo "" >> $file_name
+
+shasum report05_m2.py >> $file_name
+poetry run python3 report05_m2.py >> $file_name
+shasum ../img/p5_m2.png >> $file_name
+echo "" >> $file_name
+
+shasum report05_m3.py >> $file_name
+poetry run python3 report05_m3.py >> $file_name
+shasum ../img/p5_m3.png >> $file_name
+echo "" >> $file_name
+
+shasum report05_m5.py >> $file_name
+poetry run python3 report05_m5.py >> $file_name
+shasum ../img/p5_m5.png >> $file_name
+echo "" >> $file_name
+
+cat $file_name
